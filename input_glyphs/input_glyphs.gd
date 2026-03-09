a6fb0f1c6ff58f864c7500475c076a04d8337818
nearly-tobias <65813194+nearlytobias@users.noreply.github.com>
2026-03-09 12:10:13 +1100

    Add input glyph debug panel
    - Very simple implementation using built in scripts
    - Detects cardinal directions on keyboard or d-pad only
    - Texture source: Kenney Input Prompts (CC0)

---
+++ input_glyphs/input_glyphs.gd
@@ -0,0 +1,20 @@
+class_name InputGlyphs
+extends Resource
+
+@export_category("Input Device Icon")
+@export var keyboard_icon: Texture2D
+@export var gamepad_icon: Texture2D
+
+@export_category("Keyboard Glyphs")
+@export var kb_default: Texture2D
+@export var kb_up: Texture2D
+@export var kb_down: Texture2D
+@export var kb_left: Texture2D
+@export var kb_right: Texture2D
+
+@export_category("Controller Glyphs")
+@export var pad_default: Texture2D
+@export var pad_up: Texture2D
+@export var pad_down: Texture2D
+@export var pad_left: Texture2D
+@export var pad_right: Texture2D
